b02b7e595ab7b0cba63ba910948fce68b4a94a77
Pavel Bezdienezhnykh <Pavel.Bezdienezhnykh@zooplus.com>
2018-03-12 16:54:42 +0100

    user profile data parsing

--- src/components/ProfileObject.js
+++ src/components/ProfileObject.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default (props) =>
-    <tr>
-        <td>{props.profile.id}</td>
-        <td>{props.profile.startDate}</td>
-        <td>{props.profile.endDate}</td>
-        <td>{props.profile.object}</td>
-    </tr>
+class ProfileData extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    getPresignedUrl = () => {
+        alert(1);
+    }
+
+render() {
+    return (
+        <tr>
+            <td>{this.props.profile.id}</td>
+            <td>{this.props.profile.startDate}</td>
+            <td>{this.props.profile.endDate}</td>
+            <td>{this.props.profile.object}</td>
+            <td><button  onClick={this.getPresignedUrl}>get</button></td>
+        </tr>
+    )
+}
+}
+export default ProfileData;
