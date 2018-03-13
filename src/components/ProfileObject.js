52871c49e29baed8859e5adff2c12b74d69b9c3a
Pavel Bezdienezhnykh <Pavel.Bezdienezhnykh@zooplus.com>
2018-03-13 10:58:57 +0100

    changed data format

--- src/components/ProfileObject.js
+++ src/components/ProfileObject.js
@@ -7,7 +7,9 @@ class ProfileData extends Component {
     }
 
     getPresignedUrl = () => {
-        alert(1);
+        var bucket = this.props.profile.object.bucket;
+        var objKey = this.props.profile.object.key;
+        alert(bucket + " " + objKey);
     }
 
 render() {
@@ -16,7 +18,7 @@ render() {
             <td>{this.props.profile.id}</td>
             <td>{this.props.profile.startDate}</td>
             <td>{this.props.profile.endDate}</td>
-            <td>{this.props.profile.object}</td>
+            <td>{this.props.profile.object.name}</td>
             <td><button  onClick={this.getPresignedUrl}>get</button></td>
         </tr>
     )
